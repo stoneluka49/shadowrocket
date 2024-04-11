e0134992fe1ccdbe8ba0e4e9f8b101ba721fcccf
stoneluka49 <163701747+stoneluka49@users.noreply.github.com>
2024-04-11 14:25:54 +0800

    Update 369.js

--- 369.js
+++ 369.js
@@ -18,13 +18,45 @@
 // The availabel variables: $request.url, $request.path, $request.headers, $request.body, $prefs, $task, $notify(title, subtitle, message), console.log(message), $done(response)
 
 const myStatus = "HTTP/1.1 200 OK";
-const myHeaders = {"Connection": "Close"};
+const myHeaders = {"Connection": "keep-alive"};
 const myData = "We got you.\n\n";
 
 const myResponse = {
     status: myStatus,
     headers: myHeaders,
-    body: myData + JSON.stringify($request.headers)
+    body:{
+  "result" : {
+    "picUrl" : "https:\/\/cdn.sdzhx.com.cn\/20240302\/369cx\/4\/5c46d79a-fe26-4838-8357-d609df4df6c5.jpg",
+    "splashId" : 5047,
+    "tabThree" : {
+      "targetUrl" : "https:\/\/jngj.369cx.cn\/nativepage.html?pageId=gh_5c0ca8781705%7C",
+      "config" : "{\"viewAutoLog\":false,\"content\":\"\",\"background\":\" \",\"theme\":\"\",\"title\":\"公交商城\",\"buttons\":[]}",
+      "type" : 1,
+      "name" : "公交商城",
+      "splashId" : 2598,
+          },
+    "theme" : {
+      "enableWxLogin" : true,
+      "themeMainType" : 0,
+      "themeId" : 5,
+      "themeUrl" : "https:\/\/jngj.369cx.cn\/theme\/Version7xx\/",
+      "themeColor" : "#1e73ff",
+      "bannerAdEnable" : false,
+      "themeName" : "Version7xx"
+    },
+    "tabCenter" : {
+      "targetUrl" : "__UNI__CE3255C|10221|",
+      "config" : "{\"viewAutoLog\":false,\"content\":\"\",\"background\":\"\",\"theme\":\"\",\"title\":\"泉城云卡\",\"buttons\":[]}",
+      "type" : 8,
+      "name" : "泉城云卡",
+      "splashId" : 2494,
+      "picUrl" : ""
+    },
+  "status" : {
+    "msg" : "成功",
+    "code" : 0
+  }
+}
 };
 
 $done(myResponse);
