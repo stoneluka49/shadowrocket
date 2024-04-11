e08390d1ebd0bb1d7c529f1b486128cf713277e9
stoneluka49 <163701747+stoneluka49@users.noreply.github.com>
2024-04-11 11:33:57 +0800

    Update 369.js

--- 369.js
+++ 369.js
@@ -1,34 +1,8 @@
-{
-  "result" : {
-    "picUrl" : "https:\/\/cdn.sdzhx.com.cn\/20240302\/369cx\/4\/5c46d79a-fe26-4838-8357-d609df4df6c5.jpg",
-    "splashId" : 5047,
-    "tabThree" : {
-      "targetUrl" : "https:\/\/jngj.369cx.cn\/nativepage.html?pageId=gh_5c0ca8781705%7C",
-      "config" : "{\"viewAutoLog\":false,\"content\":\"\",\"background\":\" \",\"theme\":\"\",\"title\":\"公交商城\",\"buttons\":[]}",
-      "type" : 1,
-      "name" : "公交商城",
-      "splashId" : 2598,
-      "picUrl" : " "
-    },
-    "theme" : {
-      "enableWxLogin" : true,
-      "themeMainType" : 0,
-      "themeId" : 0,
-      "themeUrl" : "https:\/\/jngj.369cx.cn\/theme\/Version7xx\/",
-      "themeColor" : "#1e73ff",
-      "bannerAdEnable" : false,
-      "themeName" : "Version7xx"
-    },
-    "tabCenter" : {
-      "targetUrl" : "__UNI__CE3255C|10221|",
-      "config" : "{\"viewAutoLog\":false,\"content\":\"\",\"background\":\"\",\"theme\":\"\",\"title\":\"泉城云卡\",\"buttons\":[]}",
-      "type" : 8,
-      "name" : "泉城云卡",
-      "splashId" : 2494,
-      "picUrl" : ""
-    },
-  "status" : {
-    "msg" : "成功",
-    "code" : 0
-  }
-}
+let reg = '<head>';
+let str = '<head>\
+    <link rel="stylesheet" href="https://github.com/stoneluka49/shadowrocket/edit/main/369.user.css" type="text/css" />\
+    <script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let body = $response.body.replace(reg, str);
+let headers = $response.headers;
+headers['Content-Security-Policy'] = '';
+$done({ headers, body })
